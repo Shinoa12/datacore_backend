12fd257016710a2fae882859e8c3f9ff678dbdf7
martiCaceresh <82267708+martiCaceresh@users.noreply.github.com>
2024-05-08 02:01:47 -0500

    feat : Model database completed

--- datacore/admin.py
+++ datacore/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company
+from .models import  Facultad
 
 # Register your models here.
-admin.site.register(Company)
+admin.site.register(Facultad)
--- datacore/models.py
+++ datacore/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from datetime import date
-#from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class Company(models.Model):
-    name = models.CharField(max_length=50)
-    website = models.URLField(max_length=100)
-    foundation = models.PositiveIntegerField()
+from django.contrib.auth.models import AbstractUser 
 
 class Facultad(models.Model):
     id_facultad = models.AutoField(primary_key=True)
@@ -15,33 +9,47 @@ class Facultad(models.Model):
 class Especialidad(models.Model):
     id_especialidad = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=200)
-    id_facultad = models.ForeignKey(Facultad,on_delete=models.CASCADE)
+    id_facultad = models.ForeignKey(Facultad,on_delete=models.CASCADE , null=False)
 
-class Rol(models.Model):
-    id_rol = models.AutoField(primary_key=True)
+class EstadoPersona(models.Model):
+    id_estado_persona = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=200)
 
-# Aqui estaba tratando de exterder la clase auth_user
-#class CustomUser(AbstractUser):
-#    authorize_flag = models.BooleanField(default=False)
-#    motivo = models.CharField(max_length=500)   
-#    fecha_registro = models.DateTimeField(default=date.today)
-#    recursos_max = models.IntegerField(default=1)
-#    id_facultad = models.ForeignKey(Facultad,on_delete=models.CASCADE)
-#    id_especialidad = models.ForeignKey(Especialidad,on_delete=models.CASCADE)
-#    id_rol = models.ForeignKey(Rol,on_delete=models.CASCADE)
+class User(AbstractUser):
+    motivo_desautorizado = models.TextField(blank=True)
+    recursos_max = models.PositiveIntegerField(default=1)
+    id_estado_persona = models.ForeignKey(EstadoPersona, on_delete=models.CASCADE, null=False)
+    id_especialidad = models.ForeignKey(Especialidad, on_delete=models.CASCADE, null=False)
+
+class Recurso(models.Model) : 
+    id_recurso = models.AutoField(primary_key=True)
+    solicitudes_encoladas = models.IntegerField()
+    tamano_ram = models.IntegerField()
+    estado = models.BooleanField()
+    ubicacion = models.TextField(blank=True)
 
 
-class Persona(models.Model):
-    id_persona = models.AutoField(primary_key=True)
-    nombres = models.CharField(max_length=200)
-    apellidos = models.CharField(max_length=200)   
-    username = models.CharField(max_length=200)   
-    email = models.CharField(max_length=200)   
-    authorize_flag = models.BooleanField(default=False)
-    motivo = models.CharField(max_length=500)   
-    fecha_registro = models.DateTimeField(default=date.today)
-    recursos_max = models.IntegerField(default=1)
-    id_facultad = models.ForeignKey(Facultad,on_delete=models.CASCADE)
-    id_especialidad = models.ForeignKey(Especialidad,on_delete=models.CASCADE)
-    id_rol = models.ForeignKey(Rol,on_delete=models.CASCADE)
+class CPU(models.Model) : 
+    id_recurso = models.OneToOneField(Recurso, on_delete=models.CASCADE , primary_key=True)
+    nombre = models.CharField(max_length=200)
+    numero_nucleos_cpu = models.IntegerField()
+    frecuencia_cpu = models.DecimalField(decimal_places=6 , max_digits=15)
+
+class GPU(models.Model) : 
+    id_recurso = models.OneToOneField(Recurso, on_delete=models.CASCADE, primary_key=True)
+    nombre = models.CharField(max_length=200)
+    numero_nucleos_gpu = models.IntegerField()
+    tamano_vram = models.IntegerField()
+    frecuencia_gpu = models.DecimalField(decimal_places=6 , max_digits=15)
+
+class Solicitud(models.Model):
+    id_solicitud = models.AutoField(primary_key=True)
+    codigo_solicitud = models.CharField(max_length=100)
+    fecha_registro = models.DateTimeField()
+    estado_solicitud = models.CharField(max_length=100)
+    posicion_cola = models.IntegerField()
+    fecha_finalizada = models.DateTimeField()
+    parametros_ejecucion = models.TextField(blank=False)
+    fecha_procesamiento = models.DateTimeField()
+    id_recurso = models.ForeignKey(Recurso, on_delete=models.CASCADE, null=False)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
