85a2d7632915c996f356712698f178ede5ea0844
RC-UnHappy <sistemas@pideloperu.com>
2024-05-09 18:28:35 -0400

    cambios

--- datacore/urls.py
+++ datacore/urls.py
@@ -8,9 +8,10 @@ router.register(r'especialidades',views.EspecialidadViewSet , 'especialidades')
 router.register(r'estadosPersonas',views.EstadoPersonaViewSet , 'estadosPersonas')
 router.register(r'cpus',views.CPUViewSet , 'cpus')
 router.register(r'gpus',views.GPUViewSet , 'gpus')
+router.register(r'users',views.UsersViewSet , 'users')
 
 urlpatterns = [
     path('api/v1/',include(router.urls)),
-    path('api/v1/especialidades/porFacultad/<int:id_facultad>/', views.EspecialidadViewSet.as_view({'get': 'getEspecialidadesPorFacultad'}), name='especialidadesPorFacultad')
+    path('api/v1/especialidades/porFacultad/<int:id_facultad>/', views.EspecialidadViewSet.as_view({'get': 'getEspecialidadesPorFacultad'}), name='especialidadesPorFacultad'),
 ]
     
