130fdedba0abda6dec01ff28168a2a2850974dda
Rodrigo Alonso <142762612+hifirod@users.noreply.github.com>
2024-05-11 02:17:52 -0500

    Refactor views/urls

--- datacore/views.py
+++ datacore/views.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Facultad , Especialidad , EstadoPersona , CPU , GPU, User
-from .serializer import FacultadSerializer , EspecialidadSerializer , EstadoPersonaSerializer , CPUSerializer , GPUSerializer, UserSerializer
+from rest_framework.decorators import action
+from .models import Facultad, Especialidad, EstadoPersona, CPU, GPU, User
+from .serializer import (
+    FacultadSerializer,
+    EspecialidadSerializer,
+    EstadoPersonaSerializer,
+    CPUSerializer,
+    GPUSerializer,
+    UserSerializer,
+)
 
-# Create your views here.
 
 class FacultadViewSet(viewsets.ModelViewSet):
     queryset = Facultad.objects.all()
@@ -15,28 +22,27 @@ class EstadoPersonaViewSet(viewsets.ModelViewSet):
     serializer_class = EstadoPersonaSerializer
 
 
-class EspecialidadViewSet(viewsets.ModelViewSet) : 
+class EspecialidadViewSet(viewsets.ModelViewSet):
     queryset = Especialidad.objects.all()
     serializer_class = EspecialidadSerializer
 
-    #Metodo que lista todas las especialidades de una facultad
-    def getEspecialidadesPorFacultad(self, request, id_facultad):
-        especialidades = self.queryset.filter(id_facultad_id = id_facultad)
+    # Método que lista todas las especialidades de una facultad
+    def list_por_facultad(self, request, id_facultad):
+        especialidades = self.queryset.filter(id_facultad_id=id_facultad)
         serializer = self.get_serializer(especialidades, many=True)
         return Response(serializer.data)
+
 
 class CPUViewSet(viewsets.ModelViewSet):
     queryset = CPU.objects.all()
     serializer_class = CPUSerializer
 
+
 class GPUViewSet(viewsets.ModelViewSet):
     queryset = GPU.objects.all()
     serializer_class = GPUSerializer
 
+
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
-
-
-
