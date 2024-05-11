a4d7be16a0e1de43e71e0c7b601e1b67fb319b41
Rodrigo Alonso <142762612+hifirod@users.noreply.github.com>
2024-05-11 02:04:44 -0500

    Update CPU/GPU serializers

--- datacore/serializer.py
+++ datacore/serializer.py
@@ -1,36 +1,48 @@
 from rest_framework import serializers
-from .models import  Facultad , Especialidad , EstadoPersona , CPU , GPU, User
+from .models import Facultad, Especialidad, EstadoPersona, CPU, GPU, Recurso, User
 
 
 class FacultadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Facultad
-        fields = '__all__'
+        fields = "__all__"
+
 
 class EspecialidadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Especialidad
-        fields = '__all__'
+        fields = "__all__"
+
 
 class EstadoPersonaSerializer(serializers.ModelSerializer):
     class Meta:
         model = EstadoPersona
-        fields = '__all__'
+        fields = "__all__"
+
+
+class RecursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recurso
+        fields = "__all__"
+
 
 class CPUSerializer(serializers.ModelSerializer):
+    id_recurso = RecursoSerializer()
+
     class Meta:
         model = CPU
-        fields = '__all__'
+        fields = "__all__"
+
 
 class GPUSerializer(serializers.ModelSerializer):
+    id_recurso = RecursoSerializer()
+
     class Meta:
         model = GPU
-        fields = '__all__'
+        fields = "__all__"
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
-
-
-
+        fields = "__all__"
