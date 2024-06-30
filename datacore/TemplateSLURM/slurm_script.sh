e38a3be6aeb96543571848730030357406805c5e
juandv182 <a20170089@pucp.edu.pe>
2024-06-29 22:36:07 -0500

    cambios

--- datacore/TemplateSLURM/slurm_script.sh
+++ datacore/TemplateSLURM/slurm_script.sh
@@ -4,15 +4,17 @@
 #SBATCH --error=test_job_error.txt
 #SBATCH --partition={{ resource_type }}
 
-
 # Enviar inicio de ejecución al API
 curl -X POST -H "Content-Type: application/json" -d '{"status": "started"}' http://100.27.105.231:8001/datacore/api/v1/InicioProcesamientoSolicitud/{{ codigo_solicitud }}/
-# nombre_sh={{ user_bash }}
 
-# echo $nombre_sh > nombre.txt
-# echo nombre_sh
-sudo chmod +x user.sh
-./user.sh
+# Verificar si user.sh existe y tiene permisos de ejecución
+if [ -f "user.sh" ]; then
+  sudo chmod +x user.sh
+  ./user.sh
+else
+  echo "El archivo user.sh no existe."
+  exit 1
+fi
 
 # Verificar si existe la carpeta 'resultados'
 if [ -d "resultados" ]; then
@@ -28,7 +30,6 @@ mv test_job_error.txt resultados/
 zip -r resultados.zip resultados
 
 # Enviar solicitud HTTP a la API para notificar el final del proceso
-
 curl -X POST -H "Content-Type: multipart/form-data" -F "status=finished" -F "id_solicitud={{ codigo_solicitud }}" -F "file=@resultados.zip" http://100.27.105.231:8001/datacore/api/v1/FinProcesamientoSolicitud/
 
 rm -rf resultados
