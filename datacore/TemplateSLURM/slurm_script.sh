9515b93e302d47cdc28d9b096fecbba89eaa6607
BurstT <89946525+BurstT@users.noreply.github.com>
2024-06-30 10:15:15 -0500

    Update slurm_script.sh

--- datacore/TemplateSLURM/slurm_script.sh
+++ datacore/TemplateSLURM/slurm_script.sh
@@ -6,15 +6,15 @@
 
 echo "ESTO ES UNA PRUEBA"
 # Enviar inicio de ejecución al API
-response=$(curl -o /dev/null -s -w "%{http_code}\n" -X POST -H "Content-Type: application/json" -d '{"status": "started"}' http://100.27.105.231:8001/datacore/api/v1/InicioProcesamientoSolicitud/115/)
+#response=$(curl -o /dev/null -s -w "%{http_code}\n" -X POST -H "Content-Type: application/json" -d '{"status": "started"}' http://100.27.105.231:8001/datacore/api/v1/InicioProcesamientoSolicitud/115/)
 
 # Verificar el código de respuesta del primer curl
-if [ "$response" -eq 200 ]; then
-  echo "Inicio de procesamiento notificado exitosamente. Código de respuesta: $response"
-else
-  echo "Error al notificar el inicio de procesamiento. Código de respuesta: $response"
-  exit 1
-fi
+#if [ "$response" -eq 200 ]; then
+#  echo "Inicio de procesamiento notificado exitosamente. Código de respuesta: $response"
+#else
+#  echo "Error al notificar el inicio de procesamiento. Código de respuesta: $response"
+#  exit 1
+#fi
 
 
 # Verificar si user.sh existe y tiene permisos de ejecución
@@ -40,7 +40,7 @@ mv test_job_error.txt resultados/
 zip -r resultados.zip resultados
 
 # Enviar solicitud HTTP a la API para notificar el final del proceso
-curl -X POST -H "Content-Type: multipart/form-data" -F "status=finished" -F "id_solicitud={{ codigo_solicitud }}" -F "file=@resultados.zip" http://100.27.105.231:8001/datacore/api/v1/FinProcesamientoSolicitud/
+#curl -X POST -H "Content-Type: multipart/form-data" -F "status=finished" -F "id_solicitud={{ codigo_solicitud }}" -F "file=@resultados.zip" http://100.27.105.231:8001/datacore/api/v1/FinProcesamientoSolicitud/
 
-rm -rf resultados
-rm resultados.zip
+#rm -rf resultados
+#rm resultados.zip
